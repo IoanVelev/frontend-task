f0e09eb16e8dec9467a345258ebfc187640cf883
Ioan Velev <ioanvelev46@gmail.com>
2025-11-06 20:03:38 +0200

    Change title after navigating to banner creation page

--- src/pages/banners/BannerCreate.tsx
+++ src/pages/banners/BannerCreate.tsx
@@ -1,8 +1,15 @@
 import { Box } from '@mui/joy'
 import BannerForm from '../../components/banner/BannerForm'
 import { Button } from '@mui/joy'
+import { usePageData } from '../../context/page-data/page-data.context'
+import { useEffect } from 'react'
 
 export default function BannerCreate() {
+    const { setPageData } = usePageData()
+
+    useEffect(() => {
+        setPageData({title: 'Banner creation page'})
+    }, [setPageData])
     return (
         <>
             <BannerForm></BannerForm>
