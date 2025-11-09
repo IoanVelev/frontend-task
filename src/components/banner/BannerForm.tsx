a9306281fdd162ab4c5e14b0eed943623447ff12
Ioan Velev <ioanvelev46@gmail.com>
2025-11-10 01:10:34 +0200

    Update BannerForm.tsx

--- src/components/banner/BannerForm.tsx
+++ src/components/banner/BannerForm.tsx
@@ -1,17 +1,41 @@
 import { FormControl, FormLabel, Input, Stack } from '@mui/joy'
+import { useState } from 'react'
+import { BannerDto } from '../../services/dto/banner.dto';
 
-export default function BannerForm() {
+export default function BannerForm({ onSubmit }) {
+    const [values, setValues] = useState({imageUrl: '', link: ''});
+
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setValues(prev => ({...prev, [name]: value}))
+    }
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        onSubmit(values);
+    }
     return (
-        <form>
+        <form id='banner-form' onSubmit={handleSubmit}>
             <Stack spacing={5}>
                 <FormControl>
                     <FormLabel>Image URL</FormLabel>
-                    <Input placeholder="Enter image url" />
+                    <Input
+                    name='imageUrl'
+                    placeholder="Enter image url"
+                    value={values.imageUrl}
+                    onChange={handleChange}
+                    />
                 </FormControl>
 
                 <FormControl>
                     <FormLabel>Link</FormLabel>
-                    <Input placeholder="Enter destination link" />
+                    <Input 
+                    name='link'
+                    placeholder="Enter destination link" 
+                    value={values.link}
+                    onChange={handleChange}
+                    />
                 </FormControl>
             </Stack>
         </form>
