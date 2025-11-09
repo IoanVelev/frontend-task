b2f619c935059f698c51f31ae57cde848205fa9b
Ioan Velev <ioanvelev46@gmail.com>
2025-11-10 01:09:58 +0200

    Get form values on submit

--- src/pages/banners/BannerCreate.tsx
+++ src/pages/banners/BannerCreate.tsx
@@ -3,20 +3,26 @@ import BannerForm from '../../components/banner/BannerForm'
 import { Button } from '@mui/joy'
 import { usePageData } from '../../context/page-data/page-data.context'
 import { useEffect } from 'react'
+import { BannerDto } from '../../services/dto/banner.dto'
 
 export default function BannerCreate() {
     const { setPageData } = usePageData()
 
     useEffect(() => {
-        setPageData({title: 'Banner creation page'})
+        setPageData({ title: 'Banner creation page' })
     }, [setPageData])
+
+    const handleCreate = (values: BannerDto) => {
+        console.log('Create banner values:', values)
+    }
     return (
         <>
-            <BannerForm></BannerForm>
+            <BannerForm onSubmit={handleCreate} />
             <Box sx={{ mt: 5, display: 'flex', justifyContent: 'center' }}>
                 <Button
+                    form="banner-form"
                     type="submit"
-                    variant='solid'
+                    variant="solid"
                     sx={{
                         width: 'fit-content',
                         px: 3,
