e1674f8733fd622905abf60bba38fdcfe1d1a358
Ioan Velev <ioanvelev46@gmail.com>
2025-11-11 00:20:10 +0200

    Add form input validations and error messages

--- src/components/banner/BannerForm.tsx
+++ src/components/banner/BannerForm.tsx
@@ -1,19 +1,44 @@
 import { FormControl, FormLabel, Input, Stack } from '@mui/joy'
 import { useState } from 'react'
 import { BannerDto } from '../../services/dto/banner.dto';
+import { FormHelperText } from '@mui/joy';
 
 export default function BannerForm({ onSubmit }) {
     const [values, setValues] = useState({imageUrl: '', link: ''});
+    const [errors, setErrors] = useState({imageUrl: '', link: ''});
 
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
-        setValues(prev => ({...prev, [name]: value}))
+        setValues(prev => ({...prev, [name]: value}));
     }
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
-        onSubmit(values);
+
+        const newErrors = { imageUrl: '', link: ''};
+        let hasErrors = false;
+
+        const urlPattern = /^https?:\/\/.+/;
+
+        if(!urlPattern.test(values.imageUrl)) {
+            newErrors.imageUrl = 'Enter a valid URL starting with http:// or https://';
+            hasErrors = true;
+        }
+
+        if(!urlPattern.test(values.link)) {
+            newErrors.link = 'Enter a valid URL starting with http:// or https://';
+            hasErrors = true;
+        }
+
+        setErrors(newErrors);
+
+        if (!hasErrors) {
+            onSubmit(values);
+            setValues({ imageUrl: '', link: '' });
+            setErrors({ imageUrl: '', link: '' });
+        }
+        
     }
     return (
         <form id='banner-form' onSubmit={handleSubmit}>
@@ -25,7 +50,9 @@ export default function BannerForm({ onSubmit }) {
                     placeholder="Enter image url"
                     value={values.imageUrl}
                     onChange={handleChange}
+                    required
                     />
+                    {errors.imageUrl && <FormHelperText sx={{ color: 'red' }}>{errors.imageUrl}</FormHelperText>}
                 </FormControl>
 
                 <FormControl>
@@ -35,7 +62,9 @@ export default function BannerForm({ onSubmit }) {
                     placeholder="Enter destination link" 
                     value={values.link}
                     onChange={handleChange}
+                    required
                     />
+                    {errors.link && <FormHelperText sx={{ color: 'red' }}>{errors.link}</FormHelperText>}
                 </FormControl>
             </Stack>
         </form>
