51d178edc5238c0b644dfcdc20f9bf1266d15da0
Ioan Velev <ioanvelev46@gmail.com>
2025-11-11 02:05:17 +0200

    Refactor Image.tsx

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -2,24 +2,25 @@ import { AspectRatio, Skeleton } from '@mui/joy'
 import { useEffect, useState } from 'react'
 import ImageService from '../services/image.service.ts'
 
-export default function Image(props: {
-    url?: string
-}) {
+export default function Image(props: { url?: string }) {
     const [isLoaded, setIsLoaded] = useState(false)
     const [imageSrc, setImageSrc] = useState<string>()
 
     useEffect(() => {
-        if (props.url) {
-            ImageService.fetchImage(props.url)
-                .then((value) => {
-                        if (value) {
-                            setImageSrc(value)
-                            setIsLoaded(true)
-                        }
-                    },
-                )
+        if (!props.url) return
+        let cancelled = false
+
+        ImageService.fetchImage(props.url).then((value) => {
+            if (value) {
+                setImageSrc(value)
+                setIsLoaded(true)
+            }
+        })
+
+        return () => {
+            cancelled = true
         }
-    }, [props])
+    }, [props.url])
 
     useEffect(() => {
         if (imageSrc) {
@@ -36,22 +37,24 @@ export default function Image(props: {
                 loading={!isLoaded}
                 variant="overlay"
             >
-                <img
-                    src={imageSrc}
-                    srcSet={`${imageSrc} 2x`}
-                    style={{
-                        ...(isLoaded ? {} : { visibility: 'hidden' }),
-                        position: 'absolute',
-                        top: 0,
-                        left: 0,
-                        width: '100%',
-                        height: '100%',
-                        objectFit: 'cover',
-                        objectPosition: 'center',
-                    }}
-                    alt="Banner"
-                    onLoad={() => setIsLoaded(true)}
-                />
+                {imageSrc && (
+                    <img
+                        src={imageSrc}
+                        srcSet={`${imageSrc} 2x`}
+                        style={{
+                            ...(isLoaded ? {} : { visibility: 'hidden' }),
+                            position: 'absolute',
+                            top: 0,
+                            left: 0,
+                            width: '100%',
+                            height: '100%',
+                            objectFit: 'cover',
+                            objectPosition: 'center',
+                        }}
+                        alt="Banner"
+                        onLoad={() => setIsLoaded(true)}
+                    />
+                )}
             </Skeleton>
         </AspectRatio>
     )
