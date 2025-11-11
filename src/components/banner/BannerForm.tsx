776ad483b084fc47d5ddceb35ed767fe532fbbcc
Ioan Velev <ioanvelev46@gmail.com>
2025-11-11 19:06:21 +0200

    Populate the banner form inputs with banner data
    Populate the banner form inputs with banner data after clicking edit button

--- src/components/banner/BannerForm.tsx
+++ src/components/banner/BannerForm.tsx
@@ -1,70 +1,85 @@
 import { FormControl, FormLabel, Input, Stack } from '@mui/joy'
-import { useState } from 'react'
-import { BannerDto } from '../../services/dto/banner.dto';
-import { FormHelperText } from '@mui/joy';
+import { useEffect, useState } from 'react'
+import { BannerDto } from '../../services/dto/banner.dto'
+import { FormHelperText } from '@mui/joy'
 
-export default function BannerForm({ onSubmit }) {
-    const [values, setValues] = useState({imageUrl: '', link: ''});
-    const [errors, setErrors] = useState({imageUrl: '', link: ''});
+export default function BannerForm({
+    onSubmit,
+    initialValues,
+}: {
+    onSubmit: (values: BannerDto) => void
+    initialValues?: BannerDto
+}) {
+    const [values, setValues] = useState<BannerDto>(initialValues || { imageUrl: '', link: '' })
+    const [errors, setErrors] = useState({ imageUrl: '', link: '' })
 
+    useEffect(() => {
+        if (initialValues) setValues(initialValues)
+    }, [initialValues])
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        setValues(prev => ({...prev, [name]: value}));
+        const { name, value } = e.target
+        setValues((prev) => ({ ...prev, [name]: value }))
     }
 
     const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
+        e.preventDefault()
 
-        const newErrors = { imageUrl: '', link: ''};
-        let hasErrors = false;
+        const newErrors = { imageUrl: '', link: '' }
+        let hasErrors = false
 
-        const urlPattern = /^https?:\/\/.+/;
+        const urlPattern = /^https?:\/\/.+/
 
-        if(!urlPattern.test(values.imageUrl)) {
-            newErrors.imageUrl = 'Enter a valid URL starting with http:// or https://';
-            hasErrors = true;
+        if (!urlPattern.test(values.imageUrl)) {
+            newErrors.imageUrl = 'Enter a valid URL starting with http:// or https://'
+            hasErrors = true
         }
 
-        if(!urlPattern.test(values.link)) {
-            newErrors.link = 'Enter a valid URL starting with http:// or https://';
-            hasErrors = true;
+        if (!urlPattern.test(values.link)) {
+            newErrors.link = 'Enter a valid URL starting with http:// or https://'
+            hasErrors = true
         }
 
-        setErrors(newErrors);
+        setErrors(newErrors)
 
         if (!hasErrors) {
-            onSubmit(values);
-            setValues({ imageUrl: '', link: '' });
-            setErrors({ imageUrl: '', link: '' });
+            onSubmit(values)
+            setValues({ imageUrl: '', link: '' })
+            setErrors({ imageUrl: '', link: '' })
         }
-        
     }
     return (
-        <form id='banner-form' onSubmit={handleSubmit}>
+        <form
+            id="banner-form"
+            onSubmit={handleSubmit}
+        >
             <Stack spacing={5}>
                 <FormControl>
                     <FormLabel>Image URL</FormLabel>
                     <Input
-                    name='imageUrl'
-                    placeholder="Enter image url"
-                    value={values.imageUrl}
-                    onChange={handleChange}
-                    required
+                        name="imageUrl"
+                        placeholder="Enter image url"
+                        value={values.imageUrl}
+                        onChange={handleChange}
+                        required
                     />
-                    {errors.imageUrl && <FormHelperText sx={{ color: 'red' }}>{errors.imageUrl}</FormHelperText>}
+                    {errors.imageUrl && (
+                        <FormHelperText sx={{ color: 'red' }}>{errors.imageUrl}</FormHelperText>
+                    )}
                 </FormControl>
 
                 <FormControl>
                     <FormLabel>Link</FormLabel>
-                    <Input 
-                    name='link'
-                    placeholder="Enter destination link" 
-                    value={values.link}
-                    onChange={handleChange}
-                    required
+                    <Input
+                        name="link"
+                        placeholder="Enter destination link"
+                        value={values.link}
+                        onChange={handleChange}
+                        required
                     />
-                    {errors.link && <FormHelperText sx={{ color: 'red' }}>{errors.link}</FormHelperText>}
+                    {errors.link && (
+                        <FormHelperText sx={{ color: 'red' }}>{errors.link}</FormHelperText>
+                    )}
                 </FormControl>
             </Stack>
         </form>
