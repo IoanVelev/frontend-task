119f5531fa9df94135e0debdb9e13c82b0a3874b
Ioan Velev <ioanvelev46@gmail.com>
2025-11-11 21:03:43 +0200

    Inform user for successful created banner

--- src/pages/banners/BannerCreate.tsx
+++ src/pages/banners/BannerCreate.tsx
@@ -1,16 +1,18 @@
-import { Box } from '@mui/joy'
+import * as React from 'react'
+import { Box, Snackbar } from '@mui/joy'
 import BannerForm from '../../components/banner/BannerForm'
 import { Button } from '@mui/joy'
 import { usePageData } from '../../context/page-data/page-data.context'
 import { useEffect } from 'react'
 import { BannerDto } from '../../services/dto/banner.dto'
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid'
 import bannerService from '../../services/banner.service'
 import { useNavigate } from 'react-router-dom'
 
 export default function BannerCreate() {
     const { setPageData } = usePageData()
     const navigate = useNavigate()
+    const [open, setOpen] = React.useState(false)
 
     useEffect(() => {
         setPageData({ title: 'Banner creation page' })
@@ -20,17 +22,20 @@ export default function BannerCreate() {
         const newBanner: BannerDto = {
             id: uuidv4(),
             imageUrl: values.imageUrl,
-            link: values.link
+            link: values.link,
         }
 
         try {
             await bannerService.createBanner(newBanner)
-            navigate('/banners')
+            setOpen(true)
+
+            setTimeout(() => {
+                navigate('/banners')
+            }, 1000)
         } catch (error) {
             console.error('Failed to create banner.', error)
             alert('Something went wrong while creating the banner.')
         }
-        
     }
     return (
         <>
@@ -48,6 +53,16 @@ export default function BannerCreate() {
                     Create banner
                 </Button>
             </Box>
+            <Snackbar
+                open={open}
+                autoHideDuration={4000}
+                onClose={() => setOpen(false)}
+                variant="outlined"
+                color="success"
+                sx={{ mb: 5, mr: 7 }}
+            >
+                Banner successfuly created!
+            </Snackbar>
         </>
     )
 }
